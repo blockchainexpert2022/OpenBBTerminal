2607d50dd1255b655e0bc0e06e060944e9dda691
blockchainexpert2022 <114223263+blockchainexpert2022@users.noreply.github.com>
2023-09-03 17:42:50 +0200

    Update install-gitpod.sh

--- install-gitpod.sh
+++ install-gitpod.sh
@@ -6,3 +6,12 @@ rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
 ~/miniconda3/bin/conda init zsh
 
+# restart shell then
+
+conda env create -n obb --file build/conda/conda-3-9-env.yaml
+conda activate obb
+build/conda/cleanup_artifacts.sh
+poetry install -E all
+python terminal.py
+
+
